3dccfa901abbbf74fc9621a1331ad95c59f3360a
Nick Cummings <nickmcummings@gmail.com>
2020-07-11 13:27:08 -0700

    WIP - piping in comic image.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import styles from "./App.module.scss";
+
+async function getComic() {
+  let response = await fetch("http://achewood.com/comic.php?date=12252016");
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    let myBlob = await response.blob();
+
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElement("img");
+    image.src = objectURL;
+
+    const comicContainer = document.querySelector(`.${styles.Comic}`);
+    comicContainer.appendChild(image);
+  }
+}
 
 function App() {
+  // getComic().catch((e) => {
+  //   console.log(
+  //     "There has been a problem with your fetch operation: " + e.message
+  //   );
+  // });
+
+  useEffect(() => {
+    const iFrame = document.querySelector("iframe");
+    iFrame.width = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+
+    console.log(iFrame);
+    console.log(iFrame.contentWindow.document.body.scrollWidth);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <h1 className={styles.Title}>Chochacho dot Cool</h1>
       </header>
+      <div className={styles.Comic}>
+        <iframe
+          id="inlineFrameExample"
+          title="Inline Frame Example"
+          width="300"
+          height="200"
+          src="http://achewood.com/comic.php?date=12252016"
+        ></iframe>
+      </div>
     </div>
   );
 }
